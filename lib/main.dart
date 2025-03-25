66a8e82ce6550e582248d455c019260935a5a02a
leodoneDev <leodone125@gmail.com>
2025-03-25 17:43:32 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart'; // Import for LatLng
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OpenStreetMap Flutter Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('OpenStreetMap Example')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(
+            51.509364,
+            -0.128928,
+          ), // Center the map over London
+          initialZoom: 9.2,
+        ),
+        children: [
+          // Changed 'layers' to 'children'
+          TileLayer(
+            // Use TileLayerWidget instead of TileLayerOptions directly
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+          ),
+        ],
+      ),
+    );
+  }
+}
