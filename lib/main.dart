8137c28b6c9fe3e6ae9f156e199d75bee135c1ab
leodoneDev <leodone125@gmail.com>
2025-04-07 22:13:23 +0700

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -38,11 +38,11 @@ class _MapScreenState extends State<MapScreen> {
     _mapController = MapController();
     _determinePosition();
   }
-  
+
   Future<void> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
-123
+
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       if (mounted) {
